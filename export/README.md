8b086499b87c6ab7e1599b67792ade1bf44466cf
Louis Lang <138794+louislang@users.noreply.github.com>
2024-02-21 11:46:47 -0600

    All project data export (#37)
    * Initial export
    
    * Remove line from README
    
    * Update README.md
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/README.md
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/PhylumExt.toml
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Improve project data and add additional logging
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    * Update export/main.ts
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Charles Coggins <maxrake@users.noreply.github.com>

---
+++ export/README.md
@@ -0,0 +1,20 @@
+# `export` Extension for Phylum
+
+This extension will iterate through your projects and export issue data for everything.
+
+## Installation and Basic Usage
+
+Clone the repository and install the extension via the Phylum CLI:
+
+```console
+git clone https://github.com/phylum-dev/community-extensions
+phylum extension install community-extensions/export/
+```
+
+## Running
+
+To export all project data, run:
+
+```sh
+phylum export 
+```
---
+++ export/main.ts
@@ -0,0 +1,93 @@
+import { PhylumApi } from 'phylum';
+import { parse } from "https://deno.land/std@0.156.0/flags/mod.ts";
+import { MultiProgressBar } from "https://deno.land/x/progress@v1.4.4/mod.ts";
+
+
+/**
+ * Fetch the project data from the Phylum API.
+ */
+async function fetchProjectData(projectId: string, group?: string): Promise<any> {
+    try {
+        const url = group ? `groups/${group}/projects/${projectId}` : `data/projects/${projectId}`;
+        const response = await PhylumApi.fetch("v0/", url, {});
+
+        if (!response.ok) {
+            console.error(`\nFailed to fetch project data, received HTTP error: ${response.status}`);
+        } else {
+            const data = await response.json();
+            return data;
+        }
+    } catch (error) {
+        console.error("\nThere was an issue fetching the project data:", error);
+    }
+}
+
+/**
+ * Fetch all known projects accessible to the current account.
+ */
+async function fetchProjects(cursor?: string): Promise<any> {
+    const base = `/projects?paginate.limit=100`;
+    const projectsUrl = cursor ? `${base}&paginate.cursor=${cursor}` : base;
+    const response = await PhylumApi.fetch("v0/", projectsUrl, {});
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch projects, HTTP error: ${response.status}`);
+    }
+
+    const ret = await response.json();
+    const nextCursor = ret.values[ret.values.length - 1].id;
+
+    if (ret.has_more) {
+        return ret.values.concat(fetchProjects(nextCursor));
+    }
+    return ret.values;
+}
+
+// Parse CLI args
+const args = parse(Deno.args);
+
+// Collect the list of projects
+console.log("Fetching projects list");
+const projects = await fetchProjects();
+console.log(`Found ${projects.length} projects in your account\n`);
+
+const bars = new MultiProgressBar({
+    title: "Downloading project data",
+    clear: true,
+    complete: "=",
+    incomplete: "-",
+    display: "[:bar] :text :percent :time :completed/:total",
+});
+
+let completed = 0;
+let allProjects = {};
+
+// Iterate through projects and fetch project data
+for (let i = 0; i < projects.length; i++) {
+    let proj = projects[i];
+    let projectId = proj.id;
+    let groupName = proj.group_name;
+
+    let data = await fetchProjectData(projectId, groupName);
+    completed++;
+
+    if (!data) {
+        continue;
+    }
+
+    await bars.render([
+        {
+            completed: completed,
+            total: projects.length,
+            text: data.name ? data.name : "",
+            complete: "*",
+            incomplete: ".",
+        },
+    ]);
+
+    allProjects[projectId] = data;
+}
+
+console.log("\nWriting project data to disk");
+Deno.writeTextFileSync("all-projects.json", JSON.stringify(allProjects));
+console.log("\nWrote project data to all-projects.json");
